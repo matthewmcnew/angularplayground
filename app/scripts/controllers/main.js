454ff13bac82083317be3b850345f57492c44227
Matthew McNew <matthewmcnew@Matthews-MacBook-Pro.local>
2014-09-20 15:19:34 -0600

    inital scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectxInterfaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projectxInterfaceApp
+ */
+angular.module('projectxInterfaceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
