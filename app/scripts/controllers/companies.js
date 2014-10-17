01e883d8563426cad0fd11f8f5dc3f9f9153d02e
James Billings <jamesbonline@gmail.com>
2014-10-17 15:30:35 -0600

    Clean up JShint warnings. Wrap underscore in a angular service

--- app/scripts/controllers/companies.js
+++ app/scripts/controllers/companies.js
@@ -1,35 +1,36 @@
 'use strict';
 
 angular.module('projectxApp')
-  .controller('CompaniesCtrl', function ($scope, $http, $state, ENV, $q, Session) {
+  .controller('CompaniesCtrl', function ($scope, $http, $state, ENV, $q, Session, CompaniesService, _) {
 
     var accessToken = Session.getToken();
+    var id = ($state.userId === undefined || $state.userId === null) ? 1 : $state.userId;
+    $scope.company = {};
 
-    $scope.company = {
-
+    
+    $scope.$on('CompaniesService.newCompanyForm', function(event, response) {
+      $scope.fields = response.fields;
+      $scope.links = response.links;
+    });
+    
+    
+    $scope.newCompanyForm = function() { 
+      CompaniesService.createForm(id, accessToken); 
     };
     
-    if($state.current.name === 'companies.new') {
-      $http.get(ENV.apiEndpoint + 'api/users/' + $state.userId + '/companies/new' + '?access_token=' + Session.getToken())
-        .then(
-          function (response) {
-            $scope.fields = response.data.company.fields;
-            $scope.links = response.data.company.links;
-          }
-        );
-    }
-    
+
     $scope.post_link = function() {
+      console.log($scope.links);
       return _.chain($scope.links)
         .filter(function(link) { return link.rel === 'create'; })
         .pluck('href')
         .value();
-    }
+    };
 
     $scope.create = function create(){
       var deferred = $q.defer();
       $http.post(ENV.apiEndpoint + $scope.post_link() + '?access_token='+ accessToken, $scope.company).then(
-        function (response) {
+        function () {
           $state.go('companies.index');
         }, function failure(response) {
           deferred.reject(response.data.messages);
