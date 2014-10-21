8277e9383a838ac9f40dda27c9c72a677fef9877
James Billings <jamesbonline@gmail.com>
2014-10-21 11:03:53 -0600

    update session service to contain user id as well as token. api expects both to sign out a user.

--- app/scripts/services/session.js
+++ app/scripts/services/session.js
@@ -2,20 +2,29 @@
 
 angular.module('projectxApp').factory('Session', function (localStorageService) {
     var accessToken = localStorageService.get('accessToken');
+    var userId = localStorageService.get('userId');
 
     return {
-      create: function(newToken) {
-        localStorageService.set('accessToken', newToken);
-        accessToken = newToken;
+      create: function(user) {
+        localStorageService.set('accessToken', user.access_token);
+        localStorageService.set('userId', user.id);
+        accessToken = user.access_token;
+        userId = user.id;
       },
 
       destroy: function() {
         accessToken = null;
+        userId = null;
         localStorageService.remove('accessToken');
+        localStorageService.remove('userId');
       },
 
       getToken: function() {
         return accessToken;
+      },
+
+      getUserID: function() {
+        return userId;
       }
     };
   });
