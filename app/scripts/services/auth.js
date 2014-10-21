ce1adfd35491ff4d0bb4c4fde13615b62745e663
James Billings <jamesbonline@gmail.com>
2014-10-21 11:04:38 -0600

    pass in token and id to service to sign out a user.

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -4,14 +4,14 @@ angular.module('projectxApp').factory('Auth', function Auth($rootScope, $http, E
     
   // Object to hold resources for companies.
   var _api = {};
-  _api.login = $resource(ENV.apiEndpoint + 'api/sessions');
+  _api.login = $resource(ENV.apiEndpoint + 'api/sessions/:id', {id: '@id'});
   _api.signup = $resource(ENV.apiEndpoint + 'api/users');
 
   return {
 
     login: function (credentials) {
       _api.login.save(credentials).$promise.then(function(response) {
-        Session.create(response.user.access_token);
+        Session.create(response.user);
         $rootScope.$broadcast('Auth.login', response);
       }, function(error) {
         $rootScope.$broadcast('Auth.loginError', error);
@@ -23,7 +23,7 @@ angular.module('projectxApp').factory('Auth', function Auth($rootScope, $http, E
     },
 
     signout: function() {
-      _api.login.delete().$promise.then(function(response) {
+      _api.login.delete({id:Session.getUserID(), access_token: Session.getToken()}).$promise.then(function(response) {
         Session.destroy();
         $rootScope.$broadcast('Auth.signout', response);
       });
@@ -31,7 +31,7 @@ angular.module('projectxApp').factory('Auth', function Auth($rootScope, $http, E
 
     signup: function (user) {
       _api.signup.save({user: user}).$promise.then(function(response) {
-        Session.create(response.user.access_token);
+        Session.create(response.user);
         $rootScope.$broadcast('Auth.signup', response);
       }, function (response) {
         $rootScope.$broadcast('Auth.signupError', response);
