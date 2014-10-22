5d8772efb1b64fd2c211facfce4a66d1be8f19fe
James Billings <jamesbonline@gmail.com>
2014-10-22 14:34:00 -0600

    move getDatesBetween method into the angular underscore wrapper so it's available anywhere

--- app/scripts/services/timeSheetItem.js
+++ app/scripts/services/timeSheetItem.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('projectxApp').factory('TimeSheetItemService', function Auth(ENV, $resource, $rootScope, Session) {
+angular.module('projectxApp').factory('TimeSheetItemService', function Auth(ENV, $resource, $rootScope, Session, _) {
 
    // Object to hold resources for companies.
    var _api = {};
@@ -8,31 +8,9 @@ angular.module('projectxApp').factory('TimeSheetItemService', function Auth(ENV,
    _api.newLineItem = $resource(ENV.apiEndpoint + 'api/timesheets/:timesheet_id/line_items', {timesheet_id: '@timesheet_id'});
    _api.deleteLineItem = $resource(ENV.apiEndpoint + 'api/line_items/:id', {id: '@id'});
 
-	var getDates = function( d1, d2 ){
-		var weekday = new Array(7);
-		weekday[0]=  'Sun';
-		weekday[1] = 'Mon';
-		weekday[2] = 'Tues';
-		weekday[3] = 'Wed';
-		weekday[4] = 'Thur';
-		weekday[5] = 'Fri';
-		weekday[6] = 'Sat';
-		var oneDay = 24*3600*1000;
-		for (var d=[],ms=d1*1,last=d2*1;ms<last;ms+=oneDay){	
-			var date = new Date(ms);
-			d.push( {
-				fullDate: date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
-				day: weekday[date.getDay()], 
-				date: date.getDate(), 
-				month: date.getUTCMonth()+1
-				} ); 
-		}
-		return d;
-		
-	};
 
 	var prepareLineItems = function(data) {
-		var dates = getDates(new Date(data.timesheet.start_date), new Date(data.timesheet.end_date));
+		var dates = _.getDatesBetween(new Date(data.timesheet.start_date), new Date(data.timesheet.end_date));
 		data.timesheet.dates = dates;
 		return data;
 	};
@@ -42,7 +20,6 @@ angular.module('projectxApp').factory('TimeSheetItemService', function Auth(ENV,
          _api.createForm.get({'access_token': Session.getToken()}).$promise.then(function(response) {
             // broadcast success event
             var data = prepareLineItems(response);
-            console.log(data);
             $rootScope.$broadcast('TimeSheetItemService.getLineItems', data);      
 
          }, function(error) {
@@ -62,7 +39,7 @@ angular.module('projectxApp').factory('TimeSheetItemService', function Auth(ENV,
 
          }, function(error) {
             // throw error
-            throw new Error('There was an error loading the form.', error);
+            $rootScope.$broadcast('TimeSheetItemService.newLineItemError', error);
          });
       },
 
--- app/scripts/services/underscore.js
+++ app/scripts/services/underscore.js
@@ -1,6 +1,32 @@
 'use strict';
 
 var underscore = angular.module('underscore', []);
+
 underscore.factory('_', function() {
+	
+	_.getDatesBetween = function( d1, d2 ){
+		var weekday = new Array(7);
+		weekday[0]=  'Sun';
+		weekday[1] = 'Mon';
+		weekday[2] = 'Tues';
+		weekday[3] = 'Wed';
+		weekday[4] = 'Thur';
+		weekday[5] = 'Fri';
+		weekday[6] = 'Sat';
+		var oneDay = 24*3600*1000;
+		
+		for (var d=[],ms=d1*1,last=d2*1;ms<last;ms+=oneDay){	
+			var date = new Date(ms);
+			d.push( {
+				fullDate: date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
+				day: weekday[date.getDay()], 
+				date: date.getDate(), 
+				month: date.getUTCMonth()+1
+				} ); 
+		}
+		return d;
+		
+	};
+
 	return window._; // assumes underscore has already been loaded on the page
 });
