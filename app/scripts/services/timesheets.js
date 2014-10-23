48481fe14ab3c2096ba7df03aada65def6fc9981
James Billings <jamesbonline@gmail.com>
2014-10-23 14:09:50 -0600

    add timesheet submit method to timesheet service

--- app/scripts/services/timesheets.js
+++ app/scripts/services/timesheets.js
@@ -6,6 +6,7 @@ angular.module('projectxApp').factory('TimesheetsService', function Auth(ENV, $r
    var _api = {};
    _api.createForm = $resource(ENV.apiEndpoint + 'api/employees/:employee_id/timesheets/new', {employee_id:'@employee_id'});
    _api.timeSheets = $resource(ENV.apiEndpoint + 'api/employees/:employee_id/timesheets', {employee_id:'@employee_id'});
+   _api.timeSheetsSubmit = $resource(ENV.apiEndpoint + 'api/timesheets/:timesheet_id/submit', {timesheet_id:'@timesheet_id'});
 
    return {
       
@@ -39,6 +40,18 @@ angular.module('projectxApp').factory('TimesheetsService', function Auth(ENV, $r
          function(error) {
             // broadcast failure event
             $rootScope.$broadcast('TimesheetsService.submitFormError', error);
+         });
+      },
+
+      timeSheetsSubmit: function(timesheet_id, token) {
+
+         _api.timeSheetsSubmit.save({timesheet_id: timesheet_id, access_token: token}).$promise.then(function(response) {
+            // broadcast success event
+            $rootScope.$broadcast('TimesheetsService.timeSheetsSubmit', response);
+         }, 
+         function(error) {
+            // broadcast failure event
+            $rootScope.$broadcast('TimesheetsService.timeSheetsSubmitError', error);
          });
       }
    };
