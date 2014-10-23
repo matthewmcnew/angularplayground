11d697378c130044ed36da239a4bca7aef95d415
James Billings <jamesbonline@gmail.com>
2014-10-23 16:00:28 -0600

    update days method

--- app/scripts/services/underscore.js
+++ app/scripts/services/underscore.js
@@ -15,8 +15,7 @@ underscore.factory('_', function() {
 		weekday[5] = 'Fri';
 		weekday[6] = 'Sat';
 		var oneDay = 24*3600*1000;
-		d1 = (d1*1) + oneDay;
-		d2 = (d2*1) + (oneDay*2);
+		d2 = (d2*1) + oneDay;
 		
 		for (var d=[],ms=d1*1,last=d2*1;ms<last;ms+=oneDay){	
 			var date = new Date(ms);
