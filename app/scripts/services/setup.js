3e2261867dc8ee6a223fff2a801cb0332de51bb3
mattadam <mattnery@gmail.com>
2014-10-22 19:09:17 -0600

    change setup password route

--- app/scripts/services/setup.js
+++ app/scripts/services/setup.js
@@ -4,14 +4,14 @@ angular.module('projectxApp').factory('Setup', function Auth($rootScope, ENV, Se
     
 	// Object to hold resources for companies.
 	var _api = {};
-	_api.setup = $resource(ENV.apiEndpoint + '/users/:userId/setup_account/:token', {userId:'@id', token: '@tk'});
+	_api.setup = $resource(ENV.apiEndpoint + 'api/users/:userId/setup_account', {userId:'@id', token: '@tk'});
 
 	return {
 		/* Submits the form to create a new company.
 		* @param data - Form object model 
 		*/
-		setup: function(userId, token, user) {
-			_api.setup.save({userId, token}, {user}).$promise.then(function(response) {
+		setup: function(id, token, user) {
+			_api.setup.save({userId: id, access_token: token}, {user: user}).$promise.then(function(response) {
 				// broadcast success event
 				Session.create(response.user);
 				$rootScope.$broadcast('Setup.setup');
